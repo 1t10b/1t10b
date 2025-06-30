3fe30e4fb8c9fdd96dc9e6be1b8860d8e8761ede
1t10b <120970223+1t10b@users.noreply.github.com>
2025-06-30 19:05:11 +0200

    Update summer-v1-secret.lua

--- summer-v1-secret.lua
+++ summer-v1-secret.lua
@@ -1,76 +1,59 @@
+-- Load Rayfield UI Library
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
+-- Create main window
 local Window = Rayfield:CreateWindow({
-   Name = "1t10b Secret Gag Script V1",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "1t10b Secret Gag Script V1",
-   LoadingSubtitle = "by 1t10b",
-   ShowText = " ", -- for mobile users to unhide rayfield, change if you'd like
-   Theme = "AmberGlow", -- Check https://docs.sirius.menu/rayfield/configuration/themes
-
-   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
-
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
-
-   ConfigurationSaving = {
-      Enabled = false,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "1t10b-gag-secret-v1"
-   },
-
-   Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-
-   KeySystem = false, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "1t10b Secret Gag Script V1",
-      Subtitle = "Key System",
-      Note = "by 1t10b", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"levowx"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
+    Name = "Utility Menu",
+    LoadingTitle = "Utility Menu",
+    LoadingSubtitle = "by YourName",
+    ConfigurationSaving = {
+        Enabled = false,
+    },
+    KeySystem = false
 })
 
-local MainTab = Window:CreateTab("Summer Harvest", nil) -- Title, Image
-local Tab = Window:CreateTab("Misc", nil) -- Title, Image
- local Slider = Tab:CreateSlider({
-   Name = "Speed",
-   Range = {0, 100},
-   Increment = 10,
-   Suffix = "Speed",
-   CurrentValue = 10,
-   Flag = "Speed", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-         Player.Character.Humanoid.WalkSpeed = (Value)
-   end,
-})
+-- Create Tabs
+local summerTab = Window:CreateTab("SummerHarvest", nil)
+local miscTab = Window:CreateTab("Misc", nil)
 
-  local Button = Tab:CreateButton({
-   Name = "Button Example",
-   Callback = function()
-local Player = game:GetService'Players'.LocalPlayer;
-local UIS = game:GetService'UserInputService';
-
-_G.JumpHeight = 50;
-
-function Action(Object, Function) if Object ~= nil then Function(Object); end end
-
-UIS.InputBegan:connect(function(UserInput)
-    if UserInput.UserInputType == Enum.UserInputType.Keyboard and UserInput.KeyCode == Enum.KeyCode.Space then
-        Action(Player.Character.Humanoid, function(self)
-            if self:GetState() == Enum.HumanoidStateType.Jumping or self:GetState() == Enum.HumanoidStateType.Freefall then
-                Action(self.Parent.HumanoidRootPart, function(self)
-                    self.Velocity = Vector3.new(0, _G.JumpHeight, 0);
-                end)
-            end
-        end)
+-- Player Speed Slider
+miscTab:CreateSlider({
+    Name = "Player Speed",
+    Range = {8, 500},
+    Increment = 1,
+    Suffix = " WalkSpeed",
+    CurrentValue = 16,
+    Flag = "SpeedSlider",
+    Callback = function(value)
+        local player = game:GetService("Players").LocalPlayer
+        local character = player.Character or player.CharacterAdded:Wait()
+        local humanoid = character:FindFirstChildOfClass("Humanoid")
+        if humanoid then
+            humanoid.WalkSpeed = value
+        end
     end
-end)
-   end,
+})
+
+-- Infinite Jump Toggle
+miscTab:CreateToggle({
+    Name = "Infinite Jump",
+    CurrentValue = false,
+    Flag = "InfJumpToggle",
+    Callback = function(isEnabled)
+        local UIS = game:GetService("UserInputService")
+        if isEnabled then
+            _G.InfJumpConnection = UIS.JumpRequest:Connect(function()
+                local player = game:GetService("Players").LocalPlayer
+                local humanoid = player.Character and player.Character:FindFirstChildOfClass("Humanoid")
+                if humanoid then
+                    humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+                end
+            end)
+        else
+            if _G.InfJumpConnection then
+                _G.InfJumpConnection:Disconnect()
+                _G.InfJumpConnection = nil
+            end
+        end
+    end
 })
